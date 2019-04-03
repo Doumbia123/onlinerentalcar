0fa49758f276dba9ff93b6a68f9830a385633080
Doumbia <dev.doumbia225@gmail.com>
2019-04-04 04:48:36 +0530

    designing the web site

---
+++ index.php
@@ -0,0 +1,181 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!--Font awsome icon package-->
+    <link rel="stylesheet" href="vendor/node_modules/@fortawesome/fontawesome-free/css/all.css" crossorigin="anonymous">
+    <!--Font awsome icon package end-->
+    <!--animate package-->
+    <link rel="stylesheet" href="vendor/node_modules/animate.css/animate.css">
+    <!--animate package end-->
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="vendor/node_modules/bootstrap/dist/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="webroot/css/style.css">
+    <title>Online Rental Car</title>
+</head>
+<body>
+
+<!--Main menu-->
+<nav class="navbar navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+            Bootstrap
+        </a>
+        <ul class="nav justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link active text-decoration-none" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-decoration-none" href="#">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-decoration-none" href="#">Cars</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-decoration-none" href="#">Contact</a>
+            </li>
+
+        </ul>
+    </div>
+</nav>
+<!--Main menu END-->
+
+<!--Main section-->
+<div class="container-fluid mainSection">
+    <div class="row">
+        <div class="container">
+            <div class="row" style="margin-top:10%;">
+                <div class="col-sm-6">
+                    <h1 class="mainSectionTitle">CHEAP CAR RENTAL IN YOUR DESIRED DESTINATION</h1>
+                    <p class="" style="color: white;">One of our top priorities is to adjust each package we offer to our
+                        customer’s exact needs. Rental Cars / Jeeps Starting Rs 1000 / Days</p>
+                </div>
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <form action="#" method="post">
+                                <div class="form-group">
+                                    <label for="formGroupExampleInput">PICK-UP LOCATION</label>
+                                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="City, Airport, Station,etc">
+                                </div>
+                                <div class="form-group">
+                                    <label for="formGroupExampleInput2">DROP-OFF LOCATION</label>
+                                    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="City, Airport, Station,etc">
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                        <label for="inputEmail4">PICK-UP DATE</label>
+                                        <input type="date" class="form-control" id="inputEmail4">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="inputPassword4">DROP-OFF DATE</label>
+                                        <input type="date" class="form-control" id="inputPassword4">
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--Main section End-->
+
+<!--Section service offert-->
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-3 serviceoffercolor" style="border-right: #F9F9F9 solid 2px;">
+
+            <h3><i class="fas fa-globe-africa i"></i> &nbsp; 24 / 7 CAR SUPPORT</h3>
+            <br>
+            <p> Proin gravida nibh vel velit auctor
+                aliquet. Aenean sollicitudin, lorem
+                quis bibendum auctor.</p>
+        </div>
+        <div class="col-sm-3 serviceoffercolor" style="border-right: #F9F9F9 solid 2px;">
+             <h3><i class="fas fa-globe-africa i"></i> &nbsp; LOTS OF LOCATION</h3>
+            <br>
+            <p>Proin gravida nibh vel velit auctor
+                aliquet. Aenean sollicitudin, lorem
+                quis bibendum auctor.</p>
+        </div>
+        <div class="col-sm-3 serviceoffercolor" style="border-right: #F9F9F9 solid 2px;">
+             <h3><i class="fas fa-clipboard-list i" ></i> &nbsp; RESERVATION ANYTIME</h3>
+            <br>
+            <p>Proin gravida nibh vel velit auctor
+                aliquet. Aenean sollicitudin, lorem
+                quis bibendum auctor.</p>
+        </div>
+        <div class="col-sm-3 serviceoffercolor" >
+             <h3><i class="fas fa-car-side i"></i> &nbsp; RENTAL CARS</h3>
+            <br>
+            <p>Proin gravida nibh vel velit auctor
+                aliquet. Aenean sollicitudin, lorem
+                quis bibendum auctor.</p>
+        </div>
+
+    </div>
+</div>
+<!--Section service offert-->
+
+<!--Section choose your car-->
+<div class="container-fluid chooseCars">
+    <div class="row">
+        <div class="col-sm-12">
+            <span class="d-flex justify-content-center">WHAT WE OFFER</span>
+            <p class="text-center">CHOOSE YOUR CAR</p>
+            <hr class="" style="width: 10%; height:5px; color: #4F5DEC;">
+            <p class="text-center">Morbi mollis vestibulum sollicitudin. Nunc in eros a justo facilisis rutrum.
+                Aenean id ullamcorper libero <br>  Vestibulum imperdiet nibh vel magna lacinia commodo ultricies,</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="container">
+            <div class="col-sm-12">
+                <div class="col-sm-12">
+                    <ul class="nav nav-tabs justify-content-center" id="myTab" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+                        </li>
+                    </ul>
+                    <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus adipisci, animi aperiam architecto, deserunt dolorem eaque et eum exercitationem fuga laborum minima, mollitia necessitatibus nulla reiciendis sapiente sint soluta tempore.
+                        </div>
+                        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aut beatae delectus, eos inventore, magni molestias neque non obcaecati perspiciatis saepe sequi sit voluptatibus? Aliquid blanditiis corporis nesciunt. Architecto, culpa!</div>
+                        <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet beatae expedita iusto mollitia, nisi officiis placeat quidem, sunt, tempore ut vero voluptatem! Consequuntur ex facilis ipsa nostrum soluta tempore.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--Section choose your car END-->
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="vendor/node_modules/bootstrap/dist/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+</body>
+</html>

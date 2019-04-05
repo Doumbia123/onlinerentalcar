4a6a8faef01d2d3a478be002d662c6bf2a5113cf
Doumbia <dev.doumbia225@gmail.com>
2019-04-05 13:59:46 +0530

    modification de certaines parties
    Modification de la partie choose car
    Ajout de la partie how it works

---
+++ test.php
@@ -0,0 +1,161 @@
+
+<br>
+<div class="card-deck vehicule">
+    <div class="card">
+        <div class="card-header">
+            Featured
+        </div>
+        <img src="assets/img/cars/c1.png" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                <label class="btn btn-secondary active">
+                    <input type="radio" name="options" id="option1" autocomplete="off" checked> BOOK NOW</label>
+                <label class="btn btn-secondary">
+                    <input type="radio" name="options" id="option2" autocomplete="off"> DETAILS</label>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            Featured
+        </div>
+        <img src="assets/img/cars/c1.png" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                <label class="btn btn-secondary active">
+                    <input type="radio" name="options" id="option1" autocomplete="off" checked> BOOK NOW</label>
+                <label class="btn btn-secondary">
+                    <input type="radio" name="options" id="option2" autocomplete="off"> DETAILS</label>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            Featured
+        </div>
+        <img src="assets/img/cars/c1.png" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                <label class="btn btn-secondary active">
+                    <input type="radio" name="options" id="option1" autocomplete="off" checked> BOOK NOW</label>
+                <label class="btn btn-secondary">
+                    <input type="radio" name="options" id="option2" autocomplete="off"> DETAILS</label>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            Featured
+        </div>
+        <img src="assets/img/cars/c1.png" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                <label class="btn btn-secondary active">
+                    <input type="radio" name="options" id="option1" autocomplete="off" checked> BOOK NOW</label>
+                <label class="btn btn-secondary">
+                    <input type="radio" name="options" id="option2" autocomplete="off"> DETAILS</label>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="card-deck vehicule">
+    <div class="card">
+        <div class="card-header">
+            Featured
+        </div>
+        <img src="assets/img/cars/c1.png" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                <label class="btn btn-secondary active">
+                    <input type="radio" name="options" id="option1" autocomplete="off" checked> BOOK NOW</label>
+                <label class="btn btn-secondary">
+                    <input type="radio" name="options" id="option2" autocomplete="off"> DETAILS</label>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            Featured
+        </div>
+        <img src="assets/img/cars/c1.png" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                <label class="btn btn-secondary active">
+                    <input type="radio" name="options" id="option1" autocomplete="off" checked> BOOK NOW</label>
+                <label class="btn btn-secondary">
+                    <input type="radio" name="options" id="option2" autocomplete="off"> DETAILS</label>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            Featured
+        </div>
+        <img src="assets/img/cars/c1.png" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                <label class="btn btn-secondary active">
+                    <input type="radio" name="options" id="option1" autocomplete="off" checked> BOOK NOW</label>
+                <label class="btn btn-secondary">
+                    <input type="radio" name="options" id="option2" autocomplete="off"> DETAILS</label>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            Featured
+        </div>
+        <img src="assets/img/cars/c1.png" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                <label class="btn btn-secondary active">
+                    <input type="radio" name="options" id="option1" autocomplete="off" checked> BOOK NOW</label>
+                <label class="btn btn-secondary">
+                    <input type="radio" name="options" id="option2" autocomplete="off"> DETAILS</label>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+
+
+<!--Section How it works-->
+<div class="container-fluid howItWorks">
+    <h4 class="d-flex justify-content-center proposition" style="color: #4F5DEC; padding-top: 50px; font-size: 14px;">WORK PROCESS</h4>
+    <h3 class="text-center">HOW IT WORKS</h3>
+    <hr class="separate">
+    <p class="text-center">Morbi mollis vestibulum sollicitudin. Nunc in eros a justo facilisis rutrum.
+        Aenean id ullamcorper libero <br>  Vestibulum imperdiet nibh vel magna lacinia commodo ultricies,</p>
+</div>
+<!--Section How it works end-->
+.chooseCars{
+height: 500px;
+background-image: url("../../assets/system/img/background-footer.jpg");
+background-size: contain  ;
+background-position:top ;
+background-repeat:no-repeat ;
+}
